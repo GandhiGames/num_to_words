90a5e56b70b977757861ae60863353a55069c720
Robert Wells <robert.wells@gandhigames.co.uk>
2016-02-24 16:19:53 +0000

    Initial Commit

---
+++ number_to_words/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  number_to_words
+//
+//  Created by Robert Wells on 24/02/2016.
+//  Copyright © 2016 Gandhi Games. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
